0b04c51e7ab6adecfb89d1ca1067108fa71fce75
zzkzzk1996 <zekunzhang.1996@gmail.com>
2019-09-18 21:49:41 -0400

    Day18_Algorithm

---
+++ src/BinarySearch.java
@@ -0,0 +1,95 @@
+/**
+ * @program: LeetForWork
+ * @description:
+ * @author: Zekun Zhang
+ * @create: 2019-09-18 20:55
+ */
+
+public class BinarySearch {
+    /**
+     * Recursive
+     * @param nums
+     * @param low
+     * @param high
+     * @param target
+     * @return
+     */
+    public static int binarySearch(int[] nums, int low, int high, int target) {
+        if (high <= low) return -1;
+        int mid = (high - low) / 2 + low;
+        if (nums[mid] > target) {
+            return binarySearch(nums, low, mid - 1, target);
+        } else if (nums[mid] < target) {
+            return binarySearch(nums, mid + 1, low, target);
+        } else return mid;
+    }
+
+    /**
+     * Iterative1
+     * right < target < left
+     * right + 1 = left
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int binarySearch1(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;  //[left, right]
+        while (left <= right) { //left > right
+            int mid = (right - left) / 2 + left;
+            if (nums[mid] < target) {
+                left = mid + 1; //[mid + 1, right]
+            } else if (nums[mid] > target) {
+                right = mid - 1;    //[left, mid - 1]
+            } else return mid;
+        }
+        return -1;
+    }
+
+    /**
+     * Iterative2
+     * right = left > target
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int binarySearch2(int[] nums, int target) {
+        int left = 0, right = nums.length; //[left, right)
+        while (left < right) {  //left = right
+            int mid = (right - left) / 2 + left;
+            if (nums[mid] < target) {
+                left = mid + 1; //[mid + 1, right)
+            } else if (nums[mid] > target) {
+                right = mid;    //[left, mid)
+            } else return mid;
+        }
+        return -1;
+    }
+
+    /**
+     * Iterative3
+     * left < target < right
+     * left + 1 = right
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int binarySearch3(int[] nums, int target) {
+        int left = 0, right = nums.length - 1; //[left, right]
+        while (left + 1 < right) {  //left + 1 = right
+            int mid = (right - left) / 2 + left;
+            if (nums[mid] < target) {
+                left = mid; //[mid, right]
+            } else if (nums[mid] > target) {
+                right = mid;    //[left, mid]
+            } else return mid;
+        }
+        if (target == nums[left]) {
+            return left;
+        } else if (target == nums[right]) {
+            return right;
+        }
+        return -1;
+    }
+
+
+}
--- src/sortAlgorithm/BucketSort.java
+++ src/sortAlgorithm/BucketSort.java
@@ -1,5 +1,9 @@
 package sortAlgorithm;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * @program: LeetForWork
  * @description:
@@ -8,4 +12,26 @@ package sortAlgorithm;
  */
 
 public class BucketSort {
+    private static void buckerSort(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            max = Math.max(max, nums[i]);
+            min = Math.min(min, nums[i]);
+        }
+        int bucketNum = (max - min) / nums.length + 1;
+        List<List<Integer>> bucket = new ArrayList<>();
+        for (int i = 0; i < bucketNum; i++) {
+            bucket.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = (nums[i] - min) / nums.length;
+            bucket.get(num).add(nums[i]);
+        }
+
+        for (int i = 0; i < bucket.size(); i++) {
+            Collections.sort(bucket.get(i));
+        }
+    }
 }
