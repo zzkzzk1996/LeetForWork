23ebbe041e61c92e2d9fa2bc1b5c01117ac0f97b
zzkzzk1996 <zekunzhang.1996@gmail.com>
2019-11-05 13:11:46 -0500

    Day65 Tree Base

--- src/Day65/BinaryTreeLevelOrderTraversal.java
+++ src/Day65/BinaryTreeLevelOrderTraversal.java
@@ -14,7 +14,7 @@ import java.util.Queue;
 
 public class BinaryTreeLevelOrderTraversal {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> res= new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
         if (root == null) return res;
         Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
@@ -30,5 +30,20 @@ public class BinaryTreeLevelOrderTraversal {
             res.add(list);
         }
         return res;
+    }
+
+    public List<List<Integer>> levelOrder1(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+        helper(res, root, 0);
+        return res;
+    }
+
+    private void helper(List<List<Integer>> res, TreeNode root, int level) {
+        if (root == null) return;
+        if (level >= res.size()) res.add(new ArrayList<>());
+        res.get(level).add(root.val);
+        helper(res, root.left, level + 1);
+        helper(res, root.right, level + 1);
     }
 }
