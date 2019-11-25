c0cad08900ce9b27f1cd7cc9a6cf90d892414193
zzkzzk1996 <zekunzhang.1996@gmail.com>
2019-11-24 22:25:42 -0500

    Algorithm Graph TopologicalSort

---
+++ src/graph/BfsTopologicalSort.java
@@ -0,0 +1,44 @@
+package graph;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @program: LeetForWork
+ * @description:
+ * @author: Zekun Zhang
+ * @create: 2019-11-24 22:10
+ */
+
+public class BfsTopologicalSort {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        int[] res = new int[numCourses];
+        int k = 0;
+        int[] degree = new int[numCourses];
+        for (int[] pair : prerequisites) {
+            degree[pair[0]]++;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < degree.length; i++) {
+            if (degree[i] == 0) {
+                queue.offer(i);
+                res[k++] = i;
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            int pre = queue.poll();
+            for (int[] pair : prerequisites) {
+                if (pair[1] == pre) {
+                    degree[pair[0]]--;
+                    if (degree[pair[0]] == 0) {
+                        queue.offer(pair[0]);
+                        res[k++] = pair[0];
+                    }
+                }
+            }
+        }
+        return k == res.length ? res : new int[0];
+    }
+}
---
+++ src/graph/DfsTopologicalSort.java
@@ -0,0 +1,58 @@
+package graph;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * @program: LeetForWork
+ * @description:
+ * @author: Zekun Zhang
+ * @create: 2019-11-24 21:54
+ */
+
+public class DfsTopologicalSort {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
+            graph.add(new ArrayList<>());
+        }
+        for (int[] pair : prerequisites) {
+            int next = pair[0];
+            int prev = pair[1];
+            graph.get(prev).add(next);
+        }
+        HashMap<Integer, Integer> visited = new HashMap<>();
+        for (int i = 0; i < numCourses; i++) {
+            visited.put(i, 0);
+        }
+
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (visited.get(i) == 0) {
+                if (!helper(graph, visited, res, i)) {
+                    return new int[0];
+                }
+            }
+        }
+        int[] result = new int[res.size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = res.get(numCourses - i - 1);
+        }
+        return result;
+    }
+
+    private boolean helper(List<List<Integer>> graph, HashMap<Integer, Integer> visited, List<Integer> res, int i) {
+        if (visited.get(i) == 2) return true;
+        if (visited.get(i) == 1) return false;
+        visited.put(i, 1);
+        for (int j : graph.get(i)) {
+            if (!helper(graph, visited, res, j)) {
+                return false;
+            }
+        }
+        visited.put(i, 2);
+        res.add(i);
+        return true;
+    }
+}
