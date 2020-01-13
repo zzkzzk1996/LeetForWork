81c05a94c0c8a7b1822755fceb314a7060a73452
zzkzzk1996 <zekunzhang.1996@gmail.com>
2020-01-13 19:20:12 +0800

    Matrix Problem

--- src/Day78/UniquePaths.java
+++ src/Day78/UniquePaths.java
@@ -18,7 +18,7 @@ public class UniquePaths {
                     dp[i][j] = dp[i - 1][j];
                 } else if (i == 0 && j != 0) {
                     dp[i][j] = dp[i][j - 1];
-                } else if (i != 0 && j != 0) {
+                } else if (i != 0 && j != 0) {u
                     dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
                 }
 
