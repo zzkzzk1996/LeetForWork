d06359b6a2af4f67a200a45159d6e8b6941dc2a0
zzkzzk1996 <zekunzhang.1996@gmail.com>
2020-03-01 23:11:31 -0500

    New Day 13 Fixed

--- src/zDay13/ContainsDuplicateIII.java
+++ src/zDay13/ContainsDuplicateIII.java
@@ -1,5 +1,7 @@
 package zDay13;
 
+import java.util.TreeSet;
+
 /**
  * @program: LeetForWork
  * @description: 220. Contains Duplicate III
@@ -8,4 +10,30 @@ package zDay13;
  */
 
 public class ContainsDuplicateIII {
+    public boolean containsNearbyAlmostDuplicate1(int[] nums, int k, int t) {
+        if (nums == null || nums.length == 0 || k < 0 || t < 0) return false;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < Math.min(i + 1 + k, nums.length); j++) {
+                long diff = Math.abs((long)nums[i] - (long)nums[j]);
+                long lt = (long)t;
+                if (diff <= lt) return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean containsNearbyAlmostDuplicate2(int[] nums, int k, int t) {
+        if (nums == null || nums.length == 0 || k < 0 || t < 0) return false;
+        TreeSet<Long> set = new TreeSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            Long floor = set.floor((long)nums[i] + t);
+            Long ceiling = set.ceiling((long)nums[i] - t);
+            if ((floor != null && floor >= nums[i]) || (ceiling != null && ceiling <= nums[i])) return true;
+            set.add((long)nums[i]);
+            if (i >= k) {
+                set.remove((long)nums[i - k]);
+            }
+        }
+        return false;
+    }
 }
